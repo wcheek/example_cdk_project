b3c2922f2a36b56409634b7ae452c8b6453dc557
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2022-10-26 10:15:47 +0900

    connections

--- example_cdk_project/example_cdk_project_stack.py
+++ example_cdk_project/example_cdk_project_stack.py
@@ -1,4 +1,5 @@
 from aws_cdk import Stack, Duration
+from aws_cdk import aws_lambda_event_sources as lambda_event_sources
 import aws_cdk.aws_s3 as s3
 import aws_cdk.aws_lambda as _lambda
 from constructs import Construct
@@ -8,19 +9,18 @@ class ExampleCdkProjectStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
+        # ** The code that defines your stack goes here
+        # * Create input and output buckets
         input_bucket = s3.Bucket(scope=self, id="inputBucket")
         output_bucket = s3.Bucket(scope=self, id="outputBucket")
 
+        # * Create the lambda function to connect input to output
         processing_lambda = _lambda.Function(
             scope=self,
             id="processingLambda",
             runtime=_lambda.Runtime.PYTHON_3_9,
             # Path is relative to where I execute cdk
-            code=_lambda.Code.from_asset(
-                path="lambda_funcs"
-            ),
+            code=_lambda.Code.from_asset(path="lambda_funcs"),
             handler="example_processing_lambda.lambda_handler",
             environment={
                 "input_bucket": input_bucket.bucket_name,
@@ -29,3 +29,17 @@ class ExampleCdkProjectStack(Stack):
             timeout=Duration.seconds(45),
             description="Example processing lambda function.",
         )
+
+        # * Create the connection.
+        # Trigger processing_lambda when an object is added to input bucket.
+        processing_lambda.add_event_source(
+            lambda_event_sources.S3EventSource(
+                bucket=input_bucket,
+                events=[s3.EventType.OBJECT_CREATED],
+            )
+        )
+
+        # * Permission for processing_lambda to get from input bucket
+        input_bucket.grant_read(processing_lambda)
+        # * Permission for processing_lambda to save to output bucket
+        output_bucket.grant_put(processing_lambda)
--- lambda_funcs/example_processing_lambda.py
+++ lambda_funcs/example_processing_lambda.py
@@ -1,10 +1,12 @@
 import os
 import logging
 import json
+
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 INPUT_BUCKET = os.environ["input_bucket"]
 OUTPUT_BUCKET = os.environ["output_bucket"]
+
 
 def lambda_handler(event: dict, _context):
     logger.info(f"Initial event in lambda_handler: {json.dumps(event)}")
